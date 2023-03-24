09b0506ecbb042dd016fde51a74d92b306fca6ad
Kulik1987 <kulik7771@ya.ru>
2023-03-24 23:47:18 +0300

    Task Even

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,30 @@
 package hexlet.code;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 public class App {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    System.out.println("Welcome to the Brain Games!");
-    System.out.print("May I have your name? " );
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        String numberGame = Cli.getInputText();
 
-    String name = Cli.getInputText();
-    System.out.println("Hello, " + name);
-  }
+        if ("1".equals(numberGame)) {
+            System.out.println("Welcome to the Brain Games!");
+            System.out.print("May I have your name? ");
+            String name = Cli.getInputText();
+            System.out.println("Hello, " + name);
+        } else if ("2".equals(numberGame)) {
+            System.out.println("Welcome to the Brain Games!");
+            System.out.print("May I have your name? ");
+            String name = Cli.getInputText();
+            System.out.println("Hello, " + name);
+            EvenGame.beforeEvenGame(name);
+        }
+    }
+
+    private static void startedMessage() {
+
+    }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,8 +3,8 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-  public static String getInputText(){
-    Scanner sc = new Scanner(System.in);
-    return sc.next();
-  }
+    public static String getInputText() {
+        Scanner sc = new Scanner(System.in);
+        return sc.next();
+    }
 }
