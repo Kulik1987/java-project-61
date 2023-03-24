17a0b8fbab8ea46665c349ebd039b0d2e4feda60
Kulik1987 <kulik7771@ya.ru>
2023-03-24 18:18:27 +0300

    Create App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,17 @@
 package hexlet.code;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
+
     System.out.println("Welcome to the Brain Games!");
+    System.out.print("May I have your name? " );
+
+    String name = Cli.getInputText();
+    System.out.println("Hello, " + name);
   }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,10 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+  public static String getInputText(){
+    Scanner sc = new Scanner(System.in);
+    return sc.next();
+  }
+}
